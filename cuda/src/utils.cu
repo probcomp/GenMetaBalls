478116853bd9a77a7d9bb9ad288af74556760901
Matin Ghavami <mghavami@mit.edu>
2025-11-06 07:34:13 -0500

    add basic directory structure (wip)

---
+++ cuda/src/utils.cu
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <cuda_runtime.h>
+#include <iostream>
+
+#include "utils.h"
+
+void cuda_check(cudaError_t code, const char *file, int line) {
+    if (code != cudaSuccess) {
+        std::cerr << "CUDA error at " << file << ":" << line << ": "
+                  << cudaGetErrorString(code) << std::endl;
+        exit(1);
+    }
+}
