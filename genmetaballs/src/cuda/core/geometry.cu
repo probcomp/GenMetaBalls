ee36a7ee983fd267c3296eb21304c245d7cda3dc
mugamma <matin@frogrocket.ai>
2025-11-16 23:40:58 +0000

    vector addition works

--- genmetaballs/src/cuda/core/geometry.cu
+++ genmetaballs/src/cuda/core/geometry.cu
@@ -2,5 +2,5 @@
 
 Vec3D operator+(const Vec3D a, const Vec3D b)
 {
-    return {1.0f, 2.0f, 3.0f};
+    return {a.x + b.x, a.y + b.y, a.z + b.z};
 }
--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -9,5 +9,14 @@ def rng() -> np.random.Generator:
     return np.random.default_rng(0)
 
 
-def test_vec3d(rng: np.random.Generator) -> None:
+def test_vec3d_smoke() -> None:
     _gmbb.Vec3D(0, 0, 0)
+
+def test_vec3d_smoke(rng: np.random.Generator) -> None:
+    _a, _b = rng.uniform(size=3), rng.uniform(size=3)
+    a, b = _gmbb.Vec3D(*_a), _gmbb.Vec3D(*_b)
+    c = a + b
+    _c = _a + _b
+    assert all(
+        [np.isclose(c.x, _c[0]), np.isclose(c.y, _c[1]), np.isclose(c.z, _c[2])]
+    )
