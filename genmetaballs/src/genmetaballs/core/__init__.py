c90a66d01863e346fe5cf9d6d69267ba54eba224
Arijit Dasgupta <65239742+arijit-dasgupta@users.noreply.github.com>
2025-11-18 13:14:45 -0500

    Update genmetaballs python __init__
    Co-authored-by: Xiaoyan Wang <xxiaoyanwang@gmail.com>

--- genmetaballs/src/genmetaballs/core/__init__.py
+++ genmetaballs/src/genmetaballs/core/__init__.py
@@ -1,20 +1,10 @@
-import genmetaballs._genmetaballs_bindings as _gmbb
-
-# Import from the confidence submodule
-_confidence = _gmbb.confidence
-
-# Import from the math_utils submodule
-_math_utils = _gmbb.math_utils
-
-# Expose confidence classes and functions
-ThreeParameterConfidence = _confidence.ThreeParameterConfidence
-ZeroParameterConfidence = _confidence.ZeroParameterConfidence
-FiveParameterConfidence = _confidence.FiveParameterConfidence
-gpu_get_confidence = _confidence.gpu_get_confidence
-
-# Expose math_utils functions
-sigmoid = _math_utils.sigmoid
-sigmoid_vector = _math_utils.sigmoid_vector
+from genmetaballs._genmetaballs_bindings.confidence import (
+    FiveParameterConfidence,
+    ThreeParameterConfidence,
+    ZeroParameterConfidence,
+    gpu_get_confidence,
+)
+from genmetaballs._genmetaballs_bindings.math_utils import sigmoid, sigmoid_vector
 
 __all__ = [
     "ThreeParameterConfidence",
