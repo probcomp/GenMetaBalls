bc4372b3f4eee8e6b58e4d866e5b0559c58b7b00
Xiaoyan Wang <xxiaoyanwang@gmail.com>
2025-11-20 01:10:14 -0500

    Simplify Array2D test

--- tests/cpp_tests/test_utils.cu
+++ tests/cpp_tests/test_utils.cu
@@ -10,83 +10,56 @@
 
 #include "core/utils.cuh"
 
-template <typename Container>
-class HostArray2DTestFixture : public ::testing::Test {};
-
-using HostContainerTypes = ::testing::Types<std::vector<float>, std::array<float, 24>, // 4 * 6 = 24
-                                            thrust::host_vector<float>>;
-
-TYPED_TEST_SUITE(HostArray2DTestFixture, HostContainerTypes);
-
-TYPED_TEST(HostArray2DTestFixture, CreateAndAccessArray2DOnHost) {
-    uint32_t rows = 4;
-    uint32_t cols = 6;
-
-    // Initialize container - std::array is fixed size, others use size constructor
-    TypeParam data;
-    if constexpr (std::is_same_v<TypeParam, std::array<float, 24>>) {
-        data = std::array<float, 24>{};
-    } else {
-        data = TypeParam(rows * cols);
-    }
-    std::iota(data.begin(), data.end(), 0);
-
-    // create 2D view into the underlying data on CPU
-    auto array2d = Array2D<float>(data.data(), rows, cols);
-
-    EXPECT_EQ(array2d.size(), rows * cols);
-    EXPECT_EQ(array2d.extent(0), rows);
-    EXPECT_EQ(array2d.extent(1), cols);
-
-    // check the data is correct
-    for (auto i = 0; i < array2d.extent(0); i++) {
-        for (auto j = 0; j < array2d.extent(1); j++) {
-            // in C++23, we will be able to use array2d[i, j] to access the element :)
-            EXPECT_FLOAT_EQ(array2d(i, j), i * cols + j);
-        }
-    }
-}
-
 // CUDA kernel to fill Array2D with sequential values
-__global__ void fill_array2d(Array2D<float> array2d) {
-    uint32_t i = blockIdx.x * blockDim.x + threadIdx.x;
-    uint32_t j = blockIdx.y * blockDim.y + threadIdx.y;
+__global__ void fill_array2d_kernel(Array2D<float> array2d) {
+    uint32_t i = threadIdx.x;
+    uint32_t j = threadIdx.y;
 
     if (i < array2d.extent(0) && j < array2d.extent(1)) {
         array2d(i, j) = i * array2d.extent(1) + j;
     }
 }
 
-TEST(Array2DTest, CreateAndAccessArray2DOnDevice) {
-    uint32_t rows = 3;
-    uint32_t cols = 5;
-    uint32_t size = rows * cols;
+template <typename Container>
+class Array2DTestFixture : public ::testing::Test {};
 
-    // Initialize device vector
-    thrust::device_vector<float> device_data(size);
+using ContainerTypes = ::testing::Types<std::vector<float>, thrust::device_vector<float>>;
 
-    // create 2D view into the underlying data on device
-    auto array2d = Array2D<float>(thrust::raw_pointer_cast(device_data.data()), rows, cols);
+TYPED_TEST_SUITE(Array2DTestFixture, ContainerTypes);
+
+TYPED_TEST(Array2DTestFixture, CreateAndAccessArray2D) {
+    uint32_t rows = 4;
+    uint32_t cols = 6;
+
+    auto data = TypeParam(rows * cols);
+    // create 2D view into the underlying data on host or device
+    auto array2d = Array2D<float>(thrust::raw_pointer_cast(data.data()), rows, cols);
+
+    if constexpr (std::is_same_v<TypeParam, std::vector<float>>) {
+        for (auto i = 0; i < rows; i++) {
+            for (auto j = 0; j < cols; j++) {
+                array2d(i, j) = i * cols + j;
+            }
+        }
+    } else {
+        // Launch kernel to fill Array2D on device
+        // Note: we could've simply use thrust::sequence to fill the device vector,
+        // but this is a simple example to demonstrate how to pass an Array2D to a kernel.
+        fill_array2d_kernel<<<1, dim3(rows, cols)>>>(array2d);
+        CUDA_CHECK(cudaGetLastError());
+        CUDA_CHECK(cudaDeviceSynchronize());
+    }
 
     EXPECT_EQ(array2d.size(), rows * cols);
     EXPECT_EQ(array2d.extent(0), rows);
     EXPECT_EQ(array2d.extent(1), cols);
+    EXPECT_EQ(array2d.rank(), 2); // 2D array
 
-    // Launch kernel to fill Array2D on device
-    // Note: we could've simply use thrust::sequence to fill the device vector,
-    // but this is a simple example to demonstrate how to pass an Array2D to a kernel.
-    dim3 block_size(16, 16);
-    dim3 grid_size((rows + block_size.x - 1) / block_size.x,
-                   (cols + block_size.y - 1) / block_size.y);
-    fill_array2d<<<grid_size, block_size>>>(array2d);
-    CUDA_CHECK(cudaGetLastError());
-    CUDA_CHECK(cudaDeviceSynchronize());
-
-    // Copy data back to host to verify
-    thrust::host_vector<float> host_data = device_data;
-    for (auto i = 0; i < rows; i++) {
-        for (auto j = 0; j < cols; j++) {
-            EXPECT_FLOAT_EQ(host_data[i * cols + j], i * cols + j);
-        }
+    // create host vector to verify the data
+    // for std::vector, this simply duplicate the vector.
+    // for thrust::device_vector, it will copy the data to the host.
+    thrust::host_vector<float> host_data = data;
+    for (auto idx = 0; idx < rows * cols; idx++) {
+        EXPECT_FLOAT_EQ(host_data[idx], idx);
     }
 }
