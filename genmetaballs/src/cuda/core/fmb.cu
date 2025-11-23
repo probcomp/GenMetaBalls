946a9a23074f61499e1a9139faeb9b8c900b7119
mugamma <mghavami@mit.edu>
2025-11-23 05:01:26 +0000

    add FMB

--- genmetaballs/src/cuda/core/fmb.cu
+++ genmetaballs/src/cuda/core/fmb.cu
@@ -3,9 +3,9 @@
 #include "utils.cuh"
 
 CUDA_CALLABLE float FMB::quadratic_form(const Vec3D vec) const {
-    const auto shftd_vec = vec - pose.get_tran();
-    const auto rot_shftd_vec = pose.get_rot().apply(shftd_vec);
-    const auto scaled_rot_shftd_vec =
-        Vec3D(rot_shftd_vec.x / extent.x, rot_shftd_vec.y / extent.y, rot_shftd_vec.z / extent.z);
+    const auto shftd_vec = vec - pose_.get_tran();
+    const auto rot_shftd_vec = pose_.get_rot().apply(shftd_vec);
+    const auto scaled_rot_shftd_vec = Vec3D(
+        rot_shftd_vec.x / extent_.x, rot_shftd_vec.y / extent_.y, rot_shftd_vec.z / extent_.z);
     return dot(rot_shftd_vec, scaled_rot_shftd_vec);
 }
--- genmetaballs/src/genmetaballs/core/__init__.py
+++ genmetaballs/src/genmetaballs/core/__init__.py
@@ -1,4 +1,4 @@
-from genmetaballs._genmetaballs_bindings import geometry
+from genmetaballs._genmetaballs_bindings import fmb, geometry
 from genmetaballs._genmetaballs_bindings.confidence import (
     TwoParameterConfidence,
     ZeroParameterConfidence,
@@ -8,6 +8,7 @@ from genmetaballs._genmetaballs_bindings.utils import sigmoid
 __all__ = [
     "ZeroParameterConfidence",
     "TwoParameterConfidence",
+    "fmb",
     "geometry",
     "sigmoid",
 ]
