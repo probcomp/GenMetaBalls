287f387399fd4068bb8cc1c2125d947d6bfc01ca
mugamma <mghavami@mit.edu>
2025-11-25 19:34:25 +0000

    wip

--- genmetaballs/src/cuda/core/fmb.cuh
+++ genmetaballs/src/cuda/core/fmb.cuh
@@ -2,6 +2,12 @@
 
 #include <stdexcept>
 
+#include <thrust/copy.h>
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
+#include <thrust/tuple.h>
+
+
 #include "geometry.cuh"
 #include "utils.cuh"
 
@@ -14,6 +20,8 @@ private:
     float3 extent_;
 
 public:
+    FMB(): pose_{}, extent_{1.0f, 1.0f, 1.0f} {};
+
     FMB(const Pose& pose, float x_extent, float y_extent, float z_extent) noexcept(false)
         : pose_{pose} {
         if (x_extent <= 0 || y_extent <= 0 || z_extent <= 0)
@@ -21,24 +29,73 @@ public:
         extent_ = {x_extent, y_extent, z_extent};
     }
 
-    Pose get_pose() const {
+    CUDA_CALLABLE Pose get_pose() const {
         return pose_;
     }
-    float3 get_extent() const {
+    CUDA_CALLABLE float3 get_extent() const {
         return extent_;
     }
 
     CUDA_CALLABLE float quadratic_form(const Vec3D) const;
 };
 
-/*template <typename containter_template>
+
 class FMBScene {
 private:
-    containter_template<FMB> fmbs_;
-    containter_template<float> log_weights_;
+    thrust::device_vector<FMB> fmbs_;
+    thrust::device_vector<float> log_weights_;
+
+    friend class FMBSceneHostView;
 
 public:
-    FMBs(uint32_t size) : fmbs_(size), log_weights_(size) {
-        // TODO: set all log_weights_ to 0
+    
+    // constructors 
+    __host__ FMBScene(size_t size): fmbs_(size), log_weights_(size) {}
+
+    __device__ thrust::device_vector<FMB> &get_fmbs() { return fmbs_; }
+    __device__ thrust::device_vector<FMB> get_fmbs() const { return fmbs_; }
+
+    __device__ thrust::device_vector<float> &get_log_weights() { return log_weights_; }
+    __device__ thrust::device_vector<float> get_log_weights() const { return log_weights_; }
+
+    __device__ thrust::tuple<FMB, float> operator[] (const uint32_t i) {
+        return thrust::make_tuple(fmbs_[i], log_weights_[i]);
     }
-};*/
+
+    __device__ auto begin() {
+        return thrust::make_zip_iterator(fmbs_.begin(), log_weights_.begin());
+    }
+
+    __device__ auto end() {
+        return thrust::make_zip_iterator(fmbs_.end(), log_weights_.end());
+    }
+
+};
+
+class FMBSceneHostView {
+private:
+    thrust::host_vector<FMB> fmbs_;
+    thrust::host_vector<float> log_weights_;
+
+public:
+    FMBSceneHostView(FMBScene &scene) :
+        fmbs_{scene.fmbs_}, log_weights_{scene.log_weights_} {}
+
+    __device__ thrust::device_vector<FMB> get_fmbs() { return fmbs_; }
+
+    __device__ thrust::device_vector<float> get_log_weights() const { return log_weights_; }
+
+    __device__ thrust::tuple<FMB, float> operator[] (const uint32_t i) {
+        return thrust::make_tuple(fmbs_[i], log_weights_[i]);
+    }
+
+    __device__ auto begin() {
+        return thrust::make_zip_iterator(fmbs_.begin(), log_weights_.begin());
+    }
+
+    __device__ auto end() {
+        return thrust::make_zip_iterator(fmbs_.end(), log_weights_.end());
+    }
+
+};
+
---
+++ tests/cpp_tests/test_fmb.cu
@@ -0,0 +1,33 @@
+#include <cstdint>
+#include <cuda_runtime.h>
+#include <gtest/gtest.h>
+
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
+
+#include "core/fmb.cuh"
+
+__global__ void dummy_kernel(FMBScene &scene, float *tot_extent_x) {
+
+    float par_extent_x = 0;
+    
+    for(auto &[fmb, w] : scene) {
+        par_extent_x += fmb.get_extent().x;
+    }
+
+    *tot_extent_x = par_extent_x;
+
+}
+
+TEST(FMBTests, KernelRangeBasedForLoopSmokeTest) {
+
+    FMBScene dummy_scene(10);
+    thrust::device_vector<float> device_res(1);
+
+    dummy_kernel<<<1, 1>>>(dummy_scene, thrust::raw_pointer_cast(device_res.data()));
+
+    thrust::host_vector<float> host_res = device_res;
+
+    EXPECT_EQ(host_res[0], 10.0f);
+
+}
