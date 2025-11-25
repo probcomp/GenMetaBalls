2b87ea5c7705bf1a31f20daa05fa907a696f91aa
Xiaoyan Wang <xxiaoyanwang@gmail.com>
2025-11-25 15:53:20 -0500

    Add `DeviceType` to `Array2D` type annotation (#23)
    Recreating https://github.com/probcomp/GenMetaBalls/pull/21 because I
    accidentally closes it and GitHub doesn't let me reopen the PR..
    
    ----
    
    (Closes MET-14)
    
    Note that this PR is curently set to base on `xiaoyan/array2d-nanobind`
    instead of `master` so that only incremental diffs are displayed.
    
    ## Summary of Changes
    
    Similar to raw pointers, our current `Array2D` type does not record
    *where* the underlying data comes from, as the memory accessing pattern
    are roughly the same on CPU & GPU. However, mentally tacking the
    location of the memory can be error-prone. In addition, many other
    Python array/tensor libraries store the device type explicitly, and we
    won't be able easily convert to them without knowing where our memory
    is.
    
    As such, I'm adding a `DeviceType` template parameter to our `Array2D`
    to keep track of the location of the memory. With this change, we are
    finally able to take CPU/GPU buffers from Python side and return them
    correctly without running into segfault.
    
    ## Test Plans
    
    You can find examples of creating `Array2D` from CPU/GPU memory buffers
    with numpy and JAX ih the included `test_utils.py`.
    
    
    As always, to run all the tests:
    
    ```bash
    pixi run test
    ```

--- genmetaballs/src/cuda/bindings.cu
+++ genmetaballs/src/cuda/bindings.cu
@@ -10,6 +10,9 @@
 
 namespace nb = nanobind;
 
+template <typename T, MemoryLocation location>
+void bind_array2d(nb::module_& m, const char* name);
+
 NB_MODULE(_genmetaballs_bindings, m) {
 
     /*
@@ -80,23 +83,38 @@ NB_MODULE(_genmetaballs_bindings, m) {
     nb::module_ utils = m.def_submodule("utils");
     utils.def("sigmoid", sigmoid, nb::arg("x"), "Compute the sigmoid function: 1 / (1 + exp(-x))");
 
-    nb::class_<Array2D<float>>(utils, "FloatArray2D")
+    bind_array2d<float, MemoryLocation::HOST>(utils, "CPUFloatArray2D");
+    bind_array2d<float, MemoryLocation::DEVICE>(utils, "GPUFloatArray2D");
+
+} // NB_MODULE(_genmetaballs_bindings)
+
+template <typename T, MemoryLocation location>
+void bind_array2d(nb::module_& m, const char* name) {
+    using nb_device_type =
+        std::conditional_t<location == MemoryLocation::HOST, nb::device::cpu, nb::device::cuda>;
+    nb::class_<Array2D<T, location>>(m, name)
         .def_static("from_array",
-                    [](const nb::ndarray<float, nb::ndim<2>, nb::c_contig>& array) {
-                        return Array2D<float>(array.data(), array.shape(0), array.shape(1));
+                    [](const nb::ndarray<T, nb::ndim<2>, nb::c_contig, nb_device_type>& array) {
+                        return Array2D<T, location>(array.data(), array.shape(0), array.shape(1));
                     })
         // TODO: switch to the array_api in future nanobind release
         // https://nanobind.readthedocs.io/en/latest/api_extra.html#_CPPv4N8nanobind9array_apiE
         .def(
-            "numpy",
-            [](const Array2D<float>& self) {
-                return nb::ndarray<float, nb::numpy, nb::c_contig>(
+            "as_numpy",
+            [](const Array2D<T, location>& self) {
+                return nb::ndarray<T, nb::numpy, nb::c_contig, nb_device_type>(
                     self.data(), {self.num_rows(), self.num_cols()});
             },
             nb::rv_policy::reference_internal)
-        .def_prop_ro("num_rows", &Array2D<float>::num_rows)
-        .def_prop_ro("num_cols", &Array2D<float>::num_cols)
-        .def_prop_ro("ndim", &Array2D<float>::ndim)
-        .def_prop_ro("size", &Array2D<float>::size);
-
-} // NB_MODULE(_genmetaballs_bindings)
+        .def(
+            "as_jax",
+            [](const Array2D<T, location>& self) {
+                return nb::ndarray<T, nb::jax, nb::c_contig, nb_device_type>(
+                    self.data(), {self.num_rows(), self.num_cols()});
+            },
+            nb::rv_policy::reference_internal)
+        .def_prop_ro("num_rows", &Array2D<T, location>::num_rows)
+        .def_prop_ro("num_cols", &Array2D<T, location>::num_cols)
+        .def_prop_ro("ndim", &Array2D<T, location>::ndim)
+        .def_prop_ro("size", &Array2D<T, location>::size);
+}
--- tests/python_tests/test_utils.py
+++ tests/python_tests/test_utils.py
@@ -1,8 +1,10 @@
+import jax
+import jax.numpy as jnp
 import numpy as np
 import pytest
 from scipy.special import expit
 
-from genmetaballs.core import FloatArray2D, sigmoid
+from genmetaballs.core import array2d_float, sigmoid
 
 NUM_RNG_SEEDS_PER_TEST = 5
 NUM_N_VALUES_PER_TEST = 5
@@ -60,18 +62,39 @@ def test_sigmoid_edge_cases(x: float) -> None:
         assert actual <= 1.0
 
 
-def test_float_array2d_creation_and_view():
+@pytest.mark.parametrize("device", ["cpu", "gpu"])
+def test_array2d_float_creation_on_jax_devices(device: str):
     """Test creation of Array2D from a numpy array."""
     rows, cols = 4, 5
-    data = np.arange(rows * cols, dtype=np.float32).reshape((rows, cols))
-    array_2d = FloatArray2D.from_array(data)
+    data = jnp.arange(rows * cols, dtype=jnp.float32).reshape((rows, cols))
+    jax_device = jax.devices(device)[0]
+    data = jax.device_put(data, device=jax_device)
+    array_2d = array2d_float(data, device=device)
 
     assert array_2d.num_rows == rows
     assert array_2d.num_cols == cols
     assert array_2d.ndim == 2
 
     # then try converting back to numpy array via view
-    data_view = array_2d.numpy()
+    data_view = array_2d.as_jax()
+    assert data_view.device == data.device
+    assert jnp.allclose(data, data_view)
+
+    # Note: we can't test writability of shared view here since JAX arrays are immutable
+
+
+def test_float_array2d_view_numpy():
+    """Test creation of Array2D from a numpy array."""
+    rows, cols = 3, 4
+    data = np.arange(rows * cols, dtype=np.float32).reshape((rows, cols))
+    array_2d = array2d_float(data, device="cpu")
+
+    assert array_2d.num_rows == rows
+    assert array_2d.num_cols == cols
+    assert array_2d.ndim == 2
+
+    # then try converting back to numpy array via view
+    data_view = array_2d.as_numpy()
     assert np.allclose(data, data_view)
 
     # check that the view is writable and changes reflect back to original data
@@ -84,4 +107,4 @@ def test_create_invalid_array2d():
     data = np.arange(12, dtype=np.float32).reshape((3, 4))
 
     with pytest.raises(TypeError):
-        FloatArray2D.from_array(data.reshape((3, 4, 1)))  # not 2D
+        array2d_float(data.reshape((3, 4, 1)), device="cpu")  # not 2D
