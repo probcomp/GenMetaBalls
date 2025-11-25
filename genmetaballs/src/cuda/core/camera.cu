f4982ed3053f8e5095b10b55cd040cfa79947dd5
Xiaoyan Wang <xxiaoyanwang@gmail.com>
2025-11-25 00:15:23 -0500

    Compute directions for a bunch of rays

--- genmetaballs/src/cuda/core/camera.cu
+++ genmetaballs/src/cuda/core/camera.cu
@@ -1,4 +1,5 @@
 #include <cstdint>
+#include <cuda/std/ranges>
 #include <cuda_runtime.h>
 
 #include "camera.cuh"
@@ -9,4 +10,18 @@ CUDA_CALLABLE Vec3D Intrinsics::get_ray_direction(uint32_t px, uint32_t py) cons
     auto x = (static_cast<float>(px) - cx) / fx;
     auto y = (static_cast<float>(py) - cy) / fy;
     return Vec3D{x, y, -1.0f};
+}
+
+template <DeviceType device>
+CUDA_CALLABLE Array2D<Vec3D, device>& Intrinsics::get_ray_directions(Array2D<Vec3D, device> buffer,
+                                                                     uint32_t px_start,
+                                                                     uint32_t px_end,
+                                                                     uint32_t py_start,
+                                                                     uint32_t py_end) const {
+    for (auto i = max(0, py_start); i < min(height, py_end); ++i) {
+        for (auto j = max(0, px_start); j < min(width, px_end); ++j) {
+            buffer[i][j] = get_ray_direction(j, i);
+        }
+    }
+    return buffer;
 }
--- genmetaballs/src/cuda/core/camera.cuh
+++ genmetaballs/src/cuda/core/camera.cuh
@@ -14,7 +14,17 @@ struct Intrinsics {
     float cx;
     float cy;
 
-    // returns the direction of the ray going through pixel (px, py) in camera frame
-    // for efficiency, this function does not check if the pixel is within bounds
+    // Returns the direction of the ray going through pixel (px, py) in camera frame.
+    // For efficiency, this function does not check if the pixel is within bounds.
     CUDA_CALLABLE Vec3D get_ray_direction(uint32_t px, uint32_t py) const;
+
+    // Returns a 2D array of ray directions in camera frame in the specified pixel range
+    // and store them in the provided buffer. By default, the full image is used
+    // For efficiency, this function does not check if Array2D buffer has correct size
+    template <DeviceType device>
+    CUDA_CALLABLE Array2D<Vec3D, device>& get_ray_directions(Array2D<Vec3D, device> buffer,
+                                                             uint32_t px_start = 0,
+                                                             uint32_t px_end = UINT32_MAX,
+                                                             uint32_t py_start = 0,
+                                                             uint32_t py_end = UINT32_MAX) const;
 };
