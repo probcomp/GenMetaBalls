cd23a82f0d13cd4cb61582efd0b2c09e056d935e
mugamma <mghavami@mit.edu>
2025-11-26 05:30:19 +0000

    initial version

--- genmetaballs/src/cuda/core/fmb.cuh
+++ genmetaballs/src/cuda/core/fmb.cuh
@@ -12,6 +12,9 @@ private:
     // In Gaussian terms:
     // - mean: pose.tran
     // - cov: pose.rot.mat().inv() * diag(extent) * pose.rot.mat()
+    // note: note how the inverse is on the left. This means that pose
+    // corresponds to the precision matrix, i.e.
+    // prec = pose.rot.mat() * diag(1/extent) * pose.rot.mat().inv()
     Pose pose_;
     float3 extent_;
 
--- genmetaballs/src/cuda/core/intersector.cuh
+++ genmetaballs/src/cuda/core/intersector.cuh
@@ -8,5 +8,12 @@
 // implement equation (6) in the paper
 class LinearIntersector {
 
-    static CUDA_CALLABLE std::pair<float, float> intersect(const FMB& fmb, const Ray& ray) const;
+    static CUDA_CALLABLE std::pair<float, float> intersect(const FMB& fmb, const Ray& ray) const {
+        auto vecdiv = [](const Vec3D& u, const Vec3D& v) {
+            return Vec3D{u.x / v.x, u.y / v.y, u.z / v.z};
+        };
+        auto rot = fmb.get_pose().get_rot();
+        auto tmp = rot.inv().apply(vecdiv(rot.apply(ray.direction), fmb.get_extent()));
+        return dot(fmb.get_pose().get_tran() - ray.start, tmp) / dot(ray.direction, tmp);
+    }
 };
